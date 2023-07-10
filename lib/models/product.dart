ec4a57fdbec1cbc34361520785edd385c3b7d0cd
MocteSandoval <mocte.sandoval@hotmail.com>
2023-07-10 10:45:29 -0600

    Clase de producto agregado

---
+++ lib/models/product.dart
@@ -0,0 +1,17 @@
+class Product {
+  final String id;
+  final String item;
+  final double price;
+  final String categoryId;
+
+  Product({
+    required this.id,
+    required this.item, 
+    required this.price, 
+    required this.categoryId});
+
+
+  
+
+
+}
